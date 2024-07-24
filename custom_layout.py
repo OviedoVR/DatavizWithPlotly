080d03d9d9bfa930617bb5ec628dc4e065e96c3b
Vinícius Oviedo <65742412+OviedoVR@users.noreply.github.com>
2024-07-24 11:43:07 -0300

    Create custom_layout.py

---
+++ custom_layout.py
@@ -0,0 +1,25 @@
+
+# Define a custom layout dictionary
+custom_layout = {
+    'template': 'simple_white',
+    'width': 800,
+    'height': 400,
+    'font': {'family': 'Palatino', 'size': 15},
+    'font_color': 'gray',
+    'xaxis_showgrid': False,
+    'yaxis_showgrid': False,
+    'xaxis_linecolor': 'black',
+    'yaxis_linecolor': 'black',
+    'xaxis_zeroline': False,
+    'xaxis': {'showline': True, 'linecolor':'lightgray', 'tickcolor' : 'lightgray'},
+    'yaxis': {'showline': True, 'linecolor':'lightgray', 'tickcolor' : 'lightgray'}
+}
+
+# Create a custom marker style dictionary
+custom_marker_style = {
+    'size': 10,            # Adjust the size as needed
+    'line_width': 1,
+    'opacity': 0.7         # Adjust the opacity as needed
+}
+
+color_discrete_sequence = ['#ccff33', '#ff4d6d', 'cyan', 'white',  '#ffd60a']
