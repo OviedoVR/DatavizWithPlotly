253bf62bd5108c5d15923b5fdf0ed60800e20eb6
OviedoVR <oviedo.vinicius@gmail.com>
2024-07-24 17:27:20 -0300

    Update repo

--- custom_layout.py
+++ custom_layout.py
@@ -3,7 +3,7 @@
 custom_layout = {
     'template': 'simple_white',
     'width': 800,
-    'height': 400,
+    'height': 300,
     'font': {'family': 'Palatino', 'size': 15},
     'font_color': 'gray',
     'xaxis_showgrid': False,
@@ -18,8 +18,7 @@ custom_layout = {
 # Create a custom marker style dictionary
 custom_marker_style = {
     'size': 10,            # Adjust the size as needed
-    'line_width': 1,
+    'line_width': 0.08,
     'opacity': 0.7         # Adjust the opacity as needed
 }
-
-color_discrete_sequence = ['#ccff33', '#ff4d6d', 'cyan', 'white',  '#ffd60a']
+color_discrete_sequence = ['#708090', '#8A9A5B', 'crimson', '#B87333',  '#ffd60a']
