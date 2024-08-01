535db0dc80cc6dd9ac22f1f1759b0a71235de53e
Vinícius Oviedo <65742412+OviedoVR@users.noreply.github.com>
2024-08-01 10:09:40 -0300

    Update custom_layout.py

--- custom_layout.py
+++ custom_layout.py
@@ -1,43 +1,35 @@
 import plotly.express as px
 
-def apply_custom_layout(fig, title='', title_x=0.5):
-    # Define a custom layout dictionary
-    custom_layout = {
-        'template': 'simple_white',
-        'width': 800,
-        'height': 300,
-        'font': {'family': 'Palatino', 'size': 15},
-        'font_color': 'gray',
-        'xaxis_showgrid': False,
-        'yaxis_showgrid': False,
-        'xaxis_linecolor': 'black',
-        'yaxis_linecolor': 'black',
-        'xaxis_zeroline': False,
-        'xaxis': {'showline': True, 'linecolor': 'lightgray', 'tickcolor': 'lightgray'},
-        'yaxis': {'showline': True, 'linecolor': 'lightgray', 'tickcolor': 'lightgray'},
-        'title': {'text': title, 'x': title_x, 'xanchor': 'center'}
-    }
+# Define a custom layout dictionary
+custom_layout = {
+    'width': 700,
+    'height': 400,
+    'paper_bgcolor': 'white',
+    'plot_bgcolor': 'white',
+    'font': {'family': 'Palatino', 'size': 12},
+    'xaxis_showgrid': False,
+    'yaxis_showgrid': False,
+    'xaxis_linecolor': 'lightgray',
+    'yaxis_linecolor': 'lightgray',
+    'xaxis_zeroline': False,
+    'xaxis': {'showline': True}
+}
 
-    # Update the figure layout
-    fig.update_layout(**custom_layout)
+# Set the default template:
+px.defaults.template = 'simple_white'
 
-    # Update the marker style for all traces
-    custom_marker_style = {
-        'size': 10,            # Adjust the size as needed
-        'line_width': 0.08,
-        'opacity': 0.7         # Adjust the opacity as needed
-    }
+# Defining a color palette:
+color_discrete_sequence = ['#bdbf09', '#2292a4', 'lightgray', '#CE4760', '#d96c06']
 
-    for trace in fig.data:
-        if 'marker' in trace:
-            trace.marker.update(custom_marker_style)
+# Create a custom marker style dictionary
+custom_marker_style = {
+    'size': 10,            # Adjust the size as needed
+    'line_width': 1,
+    'opacity': 0.7         # Adjust the opacity as needed
+}
 
-    # Define the custom color sequence
-    color_discrete_sequence = ['#708090', '#8A9A5B', 'crimson', '#B87333', '#ffd60a']
-    fig.update_traces(marker=dict(color=color_discrete_sequence))
-
+def update_facet_axes(fig):
     # Atualizar todos os eixos de facetas
     fig.update_xaxes(showgrid=False, showline=True, linecolor='lightgray', tickcolor='lightgray')
     fig.update_yaxes(showgrid=False, showline=True, linecolor='lightgray', tickcolor='lightgray')
-    
     return fig
